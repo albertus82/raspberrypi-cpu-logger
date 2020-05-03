4314838f019a330fa01b1e15f17ea2abe5d3845a
Alberto <albertus82@users.noreply.github.com>
2020-05-03 22:41:11 +0200

    Test.

--- src/test/java/RaspberryPiCpuLoggerTest.java
+++ src/test/java/RaspberryPiCpuLoggerTest.java
@@ -38,13 +38,13 @@ class RaspberryPiCpuLoggerTest {
 	}
 
 	@AfterAll
-	@Timeout(60)
+	@Timeout(100)
 	static void afterAll(final MockServerClient client) throws IOException, InterruptedException, URISyntaxException {
 		// Test "Connection refused"
 		final InetSocketAddress remoteAddress = client.remoteAddress();
 		client.stop();
 		final URI uri = new URI("http", null, remoteAddress.getHostString(), remoteAddress.getPort(), PATH, null, null);
-		final boolean error = instance.post(HttpClient.newBuilder().build(), uri, "1234567890ABCDEF", Arrays.stream(PATHS).map(RaspberryPiCpuLoggerTest::fromResource).toArray(Path[]::new));
+		final boolean error = instance.post(HttpClient.newBuilder().build(), uri, "1234567890ABCDEF", Arrays.stream(PATHS).map(RaspberryPiCpuLoggerTest::getResourcePath).toArray(Path[]::new));
 		Assertions.assertTrue(error);
 	}
 
@@ -59,17 +59,17 @@ class RaspberryPiCpuLoggerTest {
 	}
 
 	@Test
-	@Timeout(60)
+	@Timeout(100)
 	void testRun(final MockServerClient client) throws IOException, InterruptedException, URISyntaxException {
 		final InetSocketAddress remoteAddress = client.remoteAddress();
 		client.when(new HttpRequest().withMethod("POST")).respond(new HttpResponse().withStatusCode(404));
 		final URI uri = new URI("http", null, remoteAddress.getHostString(), remoteAddress.getPort(), PATH, null, null);
-		int errors = instance.run(3, uri, 1, "1234567890ABCDEF", Arrays.stream(PATHS).map(RaspberryPiCpuLoggerTest::fromResource).toArray(Path[]::new));
+		int errors = instance.run(3, uri, 1, "1234567890ABCDEF", Arrays.stream(PATHS).map(RaspberryPiCpuLoggerTest::getResourcePath).toArray(Path[]::new));
 		Assertions.assertEquals(3, errors);
 	}
 
 	@Test
-	@Timeout(60)
+	@Timeout(100)
 	void testPost(final MockServerClient client) throws IOException, InterruptedException, URISyntaxException {
 		final InetSocketAddress remoteAddress = client.remoteAddress();
 		final URI uri = new URI("http", null, remoteAddress.getHostString(), remoteAddress.getPort(), PATH, null, null);
@@ -78,29 +78,29 @@ class RaspberryPiCpuLoggerTest {
 		final HttpRequest requestExpectation = new HttpRequest().withMethod("POST").withPath(PATH).withBody("api_key=1234567890ABCDEF&field1=" + 12345 / 1000d + "&field2=" + 654321 / 1000d);
 		final HttpResponse responseExpectation = new HttpResponse().withStatusCode(200).withBody(Integer.toString(new Random().nextInt()), MediaType.PLAIN_TEXT_UTF_8);
 		client.when(requestExpectation).respond(responseExpectation);
-		boolean error = instance.post(HttpClient.newBuilder().build(), uri, "1234567890ABCDEF", Arrays.stream(PATHS).map(RaspberryPiCpuLoggerTest::fromResource).toArray(Path[]::new));
+		boolean error = instance.post(HttpClient.newBuilder().build(), uri, "1234567890ABCDEF", Arrays.stream(PATHS).map(RaspberryPiCpuLoggerTest::getResourcePath).toArray(Path[]::new));
 		Assertions.assertFalse(error);
 
 		// Test HTTP >2xx
 		client.reset();
 		client.when(new HttpRequest().withMethod("POST")).respond(new HttpResponse().withStatusCode(300));
-		error = instance.post(HttpClient.newBuilder().build(), uri, "1234567890ABCDEF", Arrays.stream(PATHS).map(RaspberryPiCpuLoggerTest::fromResource).toArray(Path[]::new));
+		error = instance.post(HttpClient.newBuilder().build(), uri, "1234567890ABCDEF", Arrays.stream(PATHS).map(RaspberryPiCpuLoggerTest::getResourcePath).toArray(Path[]::new));
 		Assertions.assertTrue(error);
 		client.reset();
 		client.when(new HttpRequest().withMethod("POST")).respond(new HttpResponse().withStatusCode(500));
-		error = instance.post(HttpClient.newBuilder().build(), uri, "1234567890ABCDEF", Arrays.stream(PATHS).map(RaspberryPiCpuLoggerTest::fromResource).toArray(Path[]::new));
+		error = instance.post(HttpClient.newBuilder().build(), uri, "1234567890ABCDEF", Arrays.stream(PATHS).map(RaspberryPiCpuLoggerTest::getResourcePath).toArray(Path[]::new));
 		Assertions.assertTrue(error);
 
 		// Test HTTP <2xx
 		client.reset();
 		client.when(new HttpRequest().withMethod("POST")).respond(new HttpResponse().withStatusCode(101));
-		error = instance.post(HttpClient.newBuilder().build(), uri, "1234567890ABCDEF", Arrays.stream(PATHS).map(RaspberryPiCpuLoggerTest::fromResource).toArray(Path[]::new));
+		error = instance.post(HttpClient.newBuilder().build(), uri, "1234567890ABCDEF", Arrays.stream(PATHS).map(RaspberryPiCpuLoggerTest::getResourcePath).toArray(Path[]::new));
 		Assertions.assertTrue(error);
 	}
 
 	@SneakyThrows(URISyntaxException.class)
-	private static Path fromResource(final String path) {
-		return Paths.get(RaspberryPiCpuLoggerTest.class.getResource(path).toURI());
+	private static Path getResourcePath(final String resourceName) {
+		return Paths.get(RaspberryPiCpuLoggerTest.class.getResource(resourceName).toURI());
 	}
 
 }
